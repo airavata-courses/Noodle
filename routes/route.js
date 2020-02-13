e4e591ad7dd096be4f02102a2549d2d1c4514534
Nayeemullah Baig <nayeemullahbaig.93@gmail.com>
2020-02-13 17:03:39 -0500

    API gateway v1

---
+++ routes/route.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+        
+    let task = require("./eventHandlers");
+    const uuidv1 = require('uuid/v1');
+
+    app.get("/session-message",function( req,res){
+
+            let uid = uuidv1()
+            msg = req.body
+            console.log('msg = '+ msg)
+
+            if(resMap[msg['username']] != null)
+               {
+                res.send(resMap[msg['username']])
+               }
+            else
+                res.send('Error')
+        
+    });
+
+    
+    app.get("/session",function( req,res){
+
+        msg = req.body
+        id = msg['username']
+       
+        axios.get('http://localhost:5010/session-data'+ id).then(function(response) {
+                console.log(response.data);
+                res.send(response.data);
+              });
+    });
+
+    app.post("/task",function( req,res){
+            
+            let uid = uuidv1()
+            msg = req.body
+            user = msg['user']
+            let data = {"station":msg['station'],'user':user,'job':'data retrieval'}
+            
+            task.produce(data,'data-retrieve')
+
+            data = {"station":msg['station'],"uid":uid,'user':user,'job':'data retrieval',status:'Retrieval Process Started'}
+            res.send(data)
+
+    });
+
+};
