72826e366f69ad80b0716ac2a30bd8abc7259385
rut999 <parvatanenirutvik@gmail.com>
2020-02-23 16:33:41 -0500

    Blocker Changes

--- frontend/src/components/DashBoard/StatisticsView.js
+++ frontend/src/components/DashBoard/StatisticsView.js
@@ -1,22 +1,59 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import { Button } from "antd";
 import { read_cookie} from 'sfcookies';
+import axios from "axios";
+import useAxios from 'axios-hooks'
+
+var user = '';
+ 
+user = read_cookie('email_name');
+  
+
 
 const StatisticsView = props => {
+
+
   const goBack = () => {
     props.history.goBack();
   };
 
-  var email = '';
+  
 
-  email = read_cookie('email_name');
+  const [{ data, loading, error }, refetch] = useAxios(
+    'http://localhost:5050/session-message',
+    {username: user}
+  )
+
+  console.log(user);
+  
+ 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
   return (
-    <div style={{ marginTop: 120, textAlign: "center" }}>
-      <h2>Sessions View</h2>
-      <h3>{email}</h3>
-      <Button onClick={goBack}>Go back</Button>
+    <div>
+      <p>
+        <p>
+          <p>
+
+          </p>
+        </p>
+      </p>
+      <center>
+      <Button onClick={refetch}>refetch</Button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+  {/* <pre>{JSON.stringify(data,null ,5)}</pre> */}
+      </center>
+
+      <center>
+ <Button onClick={goBack}>Go back</Button>
+ </center>
     </div>
-  );
+  )
 };
+
+
+
+
 
 export default StatisticsView;
