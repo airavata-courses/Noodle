3f2486a2f0d50d53e5538e719e83f556ab50f707
Naga Anjaneyulu <nakopa@iu.edu>
2020-03-19 16:06:42 -0400

    Deployment issues #67

--- apigateway/config/kafkaconf.js
+++ apigateway/config/kafkaconf.js
@@ -1,7 +1,7 @@
 const kafka = require('kafka-node')
 const Producer = kafka.Producer
 const Consumer = kafka.Consumer
-const client = new kafka.KafkaClient({kafkaHost: "kafka-service:9092"})
+const client = new kafka.KafkaClient({kafkaHost:env.KAFKA_SERVER})
 
 producer = new Producer(client);
 consumer = new Consumer(
