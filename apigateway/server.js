b9052817af032d046930921d94b0a26c36ab3e9e
Nayeemullah Baig <nayeemullahbaig.93@gmail.com>
2020-03-19 23:14:55 -0400

    comm issues2

--- apigateway/server.js
+++ apigateway/server.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
--- usermanagement/server.js
+++ usermanagement/server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
