447b56759f8a7f13ef4a2027275018a02b49c50e
Naga Anjaneyulu <nakopa@iu.edu>
2020-03-19 22:44:32 -0400

    Deployment issues #67

--- apigateway/server.js
+++ apigateway/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (req.method == 'OPTIONS') {
-    res.header('Access-Control-Arrow-Methods', 'PUT, POST, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT, POST, PATCH, DELETE');
     return res.status(200).json({});
   }
   next();
