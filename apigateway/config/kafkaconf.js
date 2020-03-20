71fca3882a52d727c1b79efd3435f18684e799c2
Nayeemullah Baig <nayeemullahbaig.93@gmail.com>
2020-03-20 15:26:36 -0400

    deployment issues

--- apigateway/config/kafkaconf.js
+++ apigateway/config/kafkaconf.js
@@ -3,18 +3,44 @@ const Producer = kafka.Producer
 const Consumer = kafka.Consumer
 const client = new kafka.KafkaClient(process.env.KAFKA_SERVER)
 
-producer = new Producer(client);
-consumer = new Consumer(
 
-  client,
-  [{ topic: 'current-status',
-    offset:0, 
-    partition: 0 }],
-    {
-      autoCommit: false,
-      encoding: 'utf8',
-      fromOffset: true
-    }
-) 
+let producer = null
+let consumer = null
+
+
+//producer = new Producer(client);
+//consumer = new Consumer(
+
+//  client,
+//  [{ topic: 'current-status',
+//    offset:0, 
+//    partition: 0 }],
+//    {
+//      autoCommit: false,
+//      encoding: 'utf8',
+//      fromOffset: true
+//    }
+// )
+
+try {
+	producer = new Producer(client);
+	consumer = new Consumer(
+		{
+			kafkaHost: "kafka-service:9092",
+			//groupId: 'api-gateway-group',
+			autoCommit: true,
+			protocol: ["roundrobin"],
+			autoCommitIntervalMs: 5000,
+			fetchMaxWaitMs: 1000,
+			fetchMaxBytes: 1024*1024,
+			encoding: 'utf8',
+			fromOffset: "latest"
+		},
+		['current-status']
+	)
+} catch (error) {
+	console.log(error)
+	process.exit()
+}
 
 module.exports={producer,consumer}
