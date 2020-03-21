8b884b7b67a4a08b79f6ca7a151f2f719364276d
rut999 <parvatanenirutvik@gmail.com>
2020-03-20 21:05:39 -0400

    UI Enhancements 5

--- frontend/src/components/DashBoard/StatisticsView.js
+++ frontend/src/components/DashBoard/StatisticsView.js
@@ -19,7 +19,9 @@ const StatisticsView = props => {
   
 
   const [{ data, loading, error }, refetch] = useAxios(
-    'http://149.165.171.65:30002/session-message/username='+user
+    'http://149.165.171.65:30002/session-message',
+    {username: user},{headers:{
+    'Content-Type':'application/json'}}
   )
 
   console.log(user);
